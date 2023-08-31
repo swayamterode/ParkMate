3abf5ad5bcad781840136654001981b2e456777d
swayamterode <swayam.programmer@gmail.com>
2023-08-31 23:14:17 +0530

    Code:Form Validations

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,23 +1,49 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import axios from "axios";
 const Signin = () => {
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
   });
+
+  const navigate = useNavigate();
 
   const [errors, setErrors] = useState({
     email: "",
-    password: ""
+    password: "",
   });
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [loggedInUsername, setLoggedInUsername] = useState(""); // New state to store the logged-in username
 
   // Function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newErrors = {};
+    // axios call
+    axios
+      .post("http://localhost:3001/login", formData)
+      .then((res) => {
+        console.log(res.res);
+        if (res.data === "Success") {
+          setLoggedInUsername(formData.email); // Store the logged-in username
+          setShowPopup(true); // Show the popup
+          setTimeout(() => {
+            setShowPopup(false);
+            navigate("/");
+          }, 2000); // Adjust the time as needed
+        }
+        else{
+          alert("Invalid Credentials");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    const newErrors = {}; // To store errors
 
     if (!formData.email) {
       newErrors.email = "Email is required";
@@ -46,7 +72,7 @@ const Signin = () => {
     const { name, value } = event.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -54,16 +80,18 @@ const Signin = () => {
     <>
       <Navbar />
       <div className="min-h-screen bg-gray-800 flex flex-col justify-center items-center">
-        <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+        <div className="w-[400px] flex flex-col max-w-md p-6 rounded-3xl sm:p-10 dark:bg-gray-900 dark:text-gray-100">
           <div className="flex justify-center ">
-            <img src="../src/assets/" alt="logo"/>
+            <img src="https://flowbite.com/docs/images/logo.svg" alt="logo" />
           </div>
+
           <div className="mb-8 text-center">
             <h1 className="my-3 text-4xl font-bold">Sign in</h1>
             <p className="text-sm dark:text-gray-400">
               Sign in to access your account
             </p>
           </div>
+
           <form onSubmit={handleSubmit} noValidate className="space-y-12">
             <div className="space-y-4">
               <div>
@@ -75,9 +103,8 @@ const Signin = () => {
                   name="email"
                   id="email"
                   placeholder="ravishankar56@gmail.com"
-                  className={`w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 ${
-                    errors.email ? "border-red-500" : ""
-                  }`}
+                  className={`w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 ${errors.email ? "border-red-500" : ""
+                    }`}
                   value={formData.email}
                   onChange={handleChange}
                 />
@@ -103,9 +130,8 @@ const Signin = () => {
                   name="password"
                   id="password"
                   placeholder="********"
-                  className={`w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 ${
-                    errors.password ? "border-red-500" : ""
-                  }`}
+                  className={`w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 ${errors.password ? "border-red-500" : ""
+                    }`}
                   value={formData.password}
                   onChange={handleChange}
                 />
@@ -134,11 +160,24 @@ const Signin = () => {
                 </Link>
                 .
               </p>
+              {showPopup && (
+                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black opacity-90">
+                  <div className="bg-gray p-6 rounded-2xl shadow">
+                    <p className="text-green-400 font-bold text-lg mb-3 flex items-center justify-center">
+                      Welcome, {loggedInUsername}! <br></br> you have been signed in successfully!
+                    </p>
+                    <div className="flex items-center justify-center text-center">
+                      <div className="animate-spin rounded-full h-6 w-6 mr- 3 border-t-4 border-blue-600"></div>
+                      <p className="ml-2 text-white-500 ">Redirecting to home page..</p>
+                    </div>
+                  </div>
+                </div>
+              )} 
             </div>
           </form>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
+    username: "", //idhr error tha!
     email: "",
     password: "",
     confirmPassword: "",
   });
 
   const [errors, setErrors] = useState({});
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -19,6 +21,7 @@ const Signup = () => {
       [name]: value,
     }));
   };
+
   const validateForm = () => {
     const newErrors = {};
 
@@ -26,6 +29,14 @@ const Signup = () => {
       newErrors.email = "Email is required.";
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
       newErrors.email = "Invalid email format.";
+    } else if (formData.email !== formData.email.toLowerCase()) {
+      newErrors.email = "Email must be in lowercase.";
+    } else if (
+      !formData.email.endsWith("@gmail.com") &&
+      !formData.email.endsWith("@yahoo.com")
+    ) {
+      newErrors.email =
+        "Only @gmail.com and @yahoo.com email domains are allowed.";
     }
 
     if (!formData.password) {
@@ -49,10 +60,25 @@ const Signup = () => {
     return Object.keys(newErrors).length === 0;
   };
 
+  const [showPopup, setShowPopup] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validateForm()) {
       // Form is valid, perform desired action (e.g., submit to server)
+      axios
+        .post("http://localhost:3001/user_info", formData)
+        .then(() => {
+          setShowPopup(true); // Show the success popup
+          setTimeout(() => {
+            setShowPopup(false); // Hide the popup after some time
+            navigate("/login"); // Navigate to the login page
+          }, 2000); // Adjust the time as needed
+        })
+        .catch((err) => {
+          setShowPopup(false); // Hide the loading popup on error
+          console.log(err);
+        });
     }
   };
 
@@ -60,10 +86,10 @@ const Signup = () => {
     <>
       <Navbar />
       <div className="min-h-screen bg-gray-800 flex flex-col justify-center items-center">
-        <div className=" mt-20 flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+        <div className=" mt-20 w-5/6 flex flex-col max-w-md p-6 rounded-3xl sm:p-10 dark:bg-gray-900 dark:text-gray-100">
           <div className="flex justify-center items-center">
             <img
-              src="../src/assets/"
+              src="https://flowbite.com/docs/images/logo.svg"
               alt="logo"
               className=" flex justify-center"
             />
@@ -76,8 +102,30 @@ const Signup = () => {
           </div>
           <form onSubmit={handleSubmit} className="space-y-12">
             <div className="space-y-4">
+              {/* username */}
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm">
+                <div className="flex justify-between mb-2">
+                  <label htmlFor="username" className="text-md">
+                    Enter your full name
+                  </label>
+                </div>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  placeholder="Ajay Patil"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
+                  required
+                />
+                {errors.username && (
+                  <p className="text-red-500 text-sm ">{errors.username}</p>
+                )}
+              </div>
+              {/* email */}
+              <div>
+                <label htmlFor="email" className="block mb-2 text-md">
                   Email address
                 </label>
                 <input
@@ -94,9 +142,10 @@ const Signup = () => {
                   <p className="text-red-500 text-sm ">{errors.email}</p>
                 )}
               </div>
+              {/* password */}
               <div>
                 <div className="flex justify-between mb-2">
-                  <label htmlFor="password" className="text-sm">
+                  <label htmlFor="password" className="text-md">
                     Password
                   </label>
                 </div>
@@ -115,9 +164,10 @@ const Signup = () => {
                   </p>
                 )}
               </div>
+              {/* confirm password */}
               <div>
                 <div className="flex justify-between mb-2">
-                  <label htmlFor="confirmPassword" className="text-sm">
+                  <label htmlFor="confirmPassword" className="text-md">
                     Confirm Password
                   </label>
                 </div>
@@ -157,6 +207,64 @@ const Signup = () => {
                 </Link>
                 .
               </p>
+              {/* Popup */}
+              {showPopup && (
+              <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-90">
+                <div className="bg-gray p-3 rounded shadow">
+                  <div className="flex flex-row items-center justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="30"
+                      height="100"
+                      viewBox="0 0 48 48"
+                    >
+                      <linearGradient
+                        id="IMoH7gpu5un5Dx2vID39Ra_pIPl8tqh3igN_gr1"
+                        x1="9.858"
+                        x2="38.142"
+                        y1="9.858"
+                        y2="38.142"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" stop-color="#9dffce"></stop>
+                        <stop offset="1" stop-color="#50d18d"></stop>
+                      </linearGradient>
+                      <path
+                        fill="url(#IMoH7gpu5un5Dx2vID39Ra_pIPl8tqh3igN_gr1)"
+                        d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+                      ></path>
+                      <linearGradient
+                        id="IMoH7gpu5un5Dx2vID39Rb_pIPl8tqh3igN_gr2"
+                        x1="13"
+                        x2="36"
+                        y1="24.793"
+                        y2="24.793"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset=".824" stop-color="#135d36"></stop>
+                        <stop offset=".931" stop-color="#125933"></stop>
+                        <stop offset="1" stop-color="#11522f"></stop>
+                      </linearGradient>
+                      <path
+                        fill="url(#IMoH7gpu5un5Dx2vID39Rb_pIPl8tqh3igN_gr2)"
+                        d="M21.293,32.707l-8-8c-0.391-0.391-0.391-1.024,0-1.414l1.414-1.414	c0.391-0.391,1.024-0.391,1.414,0L22,27.758l10.879-10.879c0.391-0.391,1.024-0.391,1.414,0l1.414,1.414	c0.391,0.391,0.391,1.024,0,1.414l-13,13C22.317,33.098,21.683,33.098,21.293,32.707z"
+                      ></path>
+                    </svg>
+                    <p className="text-green-500 font-bold text-lg mb-3 pt-3 ml-2 ">
+                      Success! 🥳 🎉
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-center text-center">
+                    <div className="animate-spin rounded-full h-6 w-6 mr- 3 border-t-4 border-green-400"></div>
+                    <p className="ml-2 font-bold text-white">
+                      Redirecting to login page...
+                    </p>
+                  </div>
+                </div>
+              </div>
+              )}
             </div>
           </form>
         </div>
