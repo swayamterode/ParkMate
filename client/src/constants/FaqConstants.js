2b5bd748e2b1d4a072a27cc7743467cd07245476
swayamterode <swayam.programmer@gmail.com>
2023-12-27 15:30:25 +0530

    updated FAQ

--- client/src/constants/FaqConstants.js
+++ client/src/constants/FaqConstants.js
@@ -16,20 +16,20 @@ export const faqText = {
         title: "How does Park-mate handle license plate changes?",
         answer: "Park-mate makes updating license plate info easy. Choose 'New License Plate' or 'Already Registered Vehicle' on our website for a seamless transition without impacting reservations.",
     },
-    question5: {
-        title: "Can I book for multiple days or a week ahead?",
-        answer: "Yes, you can make multiple bookings in advance within a week. Plan your parking ahead with ease!",
-    },
-    question6: {
-        title: "Can I extend my parking time if needed?",
-        answer: "Currently, the capability or feature mentioned is not available. However, we are actively working on developing or implementing it for the future.",
-    },
-    question7: {
-        title: "Any specific steps to leave after my reservation ends?",
-        answer: "Exiting is easy—gate opens automatically at the end of the reservation. Contact support if needed. Please provide your feedback while exiting the parking lot.",
-    },
-    question8: {
-        title: "Can I get help on-site with Park-mate?",
-        answer: "Absolutely. If you need any assistance or have questions while using our system, look for on-site staff or feel free to contact our customer support team. We're here to help and ensure you have a trouble-free parking experience.",
-    },
+    // question5: {
+    //     title: "Can I book for multiple days or a week ahead?",
+    //     answer: "Yes, you can make multiple bookings in advance within a week. Plan your parking ahead with ease!",
+    // },
+    // question6: {
+    //     title: "Can I extend my parking time if needed?",
+    //     answer: "Currently, the capability or feature mentioned is not available. However, we are actively working on developing or implementing it for the future.",
+    // },
+    // question7: {
+    //     title: "Any specific steps to leave after my reservation ends?",
+    //     answer: "Exiting is easy—gate opens automatically at the end of the reservation. Contact support if needed. Please provide your feedback while exiting the parking lot.",
+    // },
+    // question8: {
+    //     title: "Can I get help on-site with Park-mate?",
+    //     answer: "Absolutely. If you need any assistance or have questions while using our system, look for on-site staff or feel free to contact our customer support team. We're here to help and ensure you have a trouble-free parking experience.",
+    // },
 };
